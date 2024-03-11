5ff39ffb880eba3c1fc6897104e7a100aca4629d
André Cirillo <andreecirillo@gmail.com>
2024-03-11 15:09:08 -0300

    Adding node config

--- src/database.js
+++ src/database.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
+import config from "config";
 
-const mongodbUrl = process.env.MONGODB_URL || "mongodb://localhost:27017/node-rest-api-online-store";
+const mongodbUrl = config.get('database.mongoUrl');
 
 const connect = () =>
   mongoose.connect(mongodbUrl);
